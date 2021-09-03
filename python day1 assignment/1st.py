cd53bed4e99a6fcb2f3fd0f9c553cbaa2072677a
sakshisawarkar <76632430+sakshisawarkar@users.noreply.github.com>
2021-09-03 13:52:28 +0530

    day1 assignment of python is submitted

---
+++ python day1 assignment/1st.py
@@ -0,0 +1,34 @@
+print("hello world")
+
+
+
+print('Python\'s world')
+a = 67
+b = 78
+sum = a+b
+print("sum :", sum)
+
+
+
+print ("""
+
+(\_/)
+( •,•)
+(")_(")
+
+""")
+
+
+mark = 85
+name = "SHREYA"
+age = 20
+print("the marks of", name, "are", mark, "and age is", age)
+
+print("the marks of %s are %.3f and is %d"%(name,mark,age))
+
+print(f"The marks of {name} is {mark} amd age is {age}")
+
+
+
+
+
