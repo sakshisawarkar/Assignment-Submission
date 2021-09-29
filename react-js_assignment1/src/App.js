e679abe6054048a071e01cf1f2297b2808c8e1a9
sakshisawarkar <76632430+sakshisawarkar@users.noreply.github.com>
2021-09-29 14:35:33 +0530

    react-js-day1 assignment submitted

---
+++ react-js_assignment1/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Title from './Title';
+import Product from './Product';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Guys</h1>
+      <Title />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-js_assignment1/src/Product.js
@@ -0,0 +1,14 @@
+import react,{Component} from "react";
+
+class Product extends Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Laptop</h1>
+            </div>
+        )
+    }
+}
+
+export default Product;
